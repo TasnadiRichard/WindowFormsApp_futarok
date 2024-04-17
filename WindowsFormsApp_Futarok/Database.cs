79ed615c67fd7e0ea0c0ffc6f902c2c682db08b3
TasnadiRichard <112942145+TasnadiRichard@users.noreply.github.com>
2024-04-17 14:40:44 +0200

    Add files via upload

---
+++ WindowsFormsApp_Futarok/Database.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp_Futarok
+{
+    internal class Database
+    {
+        private MySqlConnection connection;
+        private MySqlCommand command;
+
+        public Database() 
+        {
+            MySqlConnectionStringBuilder futar = new MySqlConnectionStringBuilder();
+            futar.Server = "localhost";
+            futar.UserID = "root";
+            futar.Password = "";
+            futar.Database = "pizza";
+            futar.CharacterSet = "utf8";
+
+            connection = new MySqlConnection(futar.ConnectionString);
+            command = connection.CreateCommand();
+        }
+
+        public void Lekeres(string fnev)
+        {
+            command.CommandText = "SELECT pizza.fazon FROM futar WHERE futar.fnev=@fnev";
+            command.Parameters.Clear();
+            command.Parameters.AddWithValue("@fnev", fnev);
+            MySqlDataReader reader = command.ExecuteReader();
+        }
+
+        public void Modositas (int ftel, string fazon)
+        {
+            connection.Open();
+            command.CommandText = "UPDATE `futar` SET `ftel` = ftel - @ftel WHERE `fazon` = @fazon";
+            command.Parameters.AddWithValue("@ftel", ftel);
+            command.Parameters.AddWithValue("@fazon", fazon);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }      
+    }
+}
---
+++ WindowsFormsApp_Futarok/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Futarok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Database db = new Database();
+            int ftel = 0;
+            string fazon = "fazon";
+            db.Modositas(ftel, fazon);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> adatok = new List<string>();
+            foreach (string adat in adatok)
+            {
+                listBox_Adatok.Items.Add(adat);
+            }
+        }
+    }
+}
